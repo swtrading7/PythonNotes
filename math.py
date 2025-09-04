7efe75b363190bf3602ead7f85724c44e71d4650
swtrading7 <stasszhupanyk10@gmail.com>
2025-09-04 23:50:06 +0100

    math.py

---
+++ math.py
@@ -0,0 +1,40 @@
+import math
+
+# friends = 2
+
+
+# friends = friends + 1 = addtion operator ( addition )
+# friends += 1 #augmented assignment operators
+
+#friends = friends - 2 ( subtraction )
+#friends -= 2
+
+#friends = friends * 3 ( multiplication )
+#friends *= 3
+
+#friends = friends / 2 ( division )
+#friends /= 2
+
+#friends = friends ** 2 ( to the power of 2 )  ( exponentiation )
+#friends **= 2
+
+#remainder = friends %  3 ( remainder is the last one remaining ) ( MODULUS )
+# if 10 / 3 = 1, 1 is the remainder ( the remaining )
+
+
+x = 5.66
+y = -2
+z = 3
+
+# result = round(x) #rounds the function
+# result = abs(y)
+#result = pow(4,3) # to the power of
+# result = max(x,y,z) #maximum value between x,y & z
+# result = min(x,y,z) #minimum value between x,y & z
+# import math | print(math.pi)
+# print(math.e)
+#result = math.sqrt() # Square Root of X
+#result = math.ceil() # rounds the number UP
+#result = math.floor() # rounds the number DOWN
+
+print(result)

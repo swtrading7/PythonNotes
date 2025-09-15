cd4da0f7a81de3727a168572107a6f393cce2140
swtrading7 <stasszhupanyk10@gmail.com>
2025-09-15 10:01:34 +0100

    conditional expressions.py

---
+++ conditional expressions.py
@@ -0,0 +1,21 @@
+#conditional expression = A one-line shortcut for the if-else statement (ternary operator)
+#                       Print or Assign one of two values based on a condition
+#                       X if condition else Y
+
+#num = 5
+
+#print("Positive" if num > 0 else "Negative")
+
+#num  = 6
+#print("Positive") if num > 0 else print("Negative")
+#result = "Even" if num % 2 == 0 else "Odd"
+# = a if a > b else b
+#min_num = a if a < b else b
+#priunt(max_num,min_num)
+
+#print(result)
+
+age = 18
+status = "Adult" if age >= 18 else "Child"
+
+print(status)
